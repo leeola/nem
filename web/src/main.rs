e626dd9564f08b029f1974c4883df2de709f2266
Lee Olayvar <leeola@fastmail.com>
2019-10-07 16:05:10 -0700

    feat: introduce moxie in test server

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,10 +1,24 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-use rocket::{get, routes};
+use moxie_dom::*;
+use rocket::{get, response::content::Html, routes};
+
+#[topo::aware]
+fn simple_list() {
+  let items = vec!["foo", "bar"];
+  moxie::mox! {
+    <ul>{
+      for item in items {
+        moxie::mox!(<li>{% "{}", item }</li>)
+      }
+    }</ul>
+  }
+}
 
 #[get("/")]
-fn index() -> &'static str {
-  "Hello, world!"
+fn index() -> Html<String> {
+  let res = moxie_dom::render_html(move || simple_list!());
+  Html(res)
 }
 
 fn main() {
