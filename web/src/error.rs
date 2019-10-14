23a89aec372d6a7e671e8062c867b913479ac0a0
Lee Olayvar <leeola@fastmail.com>
2019-10-14 13:56:45 -0700

    feat: header layout

---
+++ web/src/error.rs
@@ -0,0 +1,20 @@
+use {
+  rocket::{
+    response::{self, Responder},
+    Request, Response,
+  },
+  std::io::Cursor,
+};
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+  Internal(String),
+}
+
+impl<'r> Responder<'r> for Error {
+  fn respond_to(self, _: &Request) -> response::Result<'r> {
+    Response::build().sized_body(Cursor::new("oops")).ok()
+  }
+}
--- web/src/main.rs
+++ web/src/main.rs
@@ -1,12 +1,27 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-use rocket::{get, routes};
+pub mod error;
+pub mod state;
+
+use {
+  self::{
+    error::{Error, Result},
+    state::template::Template,
+  },
+  rocket::{get, response::content::Html, routes, State},
+  rocket_contrib::serve::StaticFiles,
+};
 
 #[get("/")]
-fn index() -> &'static str {
-  "Hello, world!"
+fn index(tmpl: State<Template>) -> std::result::Result<Html<String>, Error> {
+  let tmpl_s = tmpl.render("index", ())?;
+  Ok(Html(tmpl_s))
 }
 
 fn main() {
-  rocket::ignite().mount("/", routes![index]).launch();
+  rocket::ignite()
+    .mount("/public", StaticFiles::from("./public"))
+    .manage(Template::new("./templates").expect("templates failed to initialize"))
+    .mount("/", routes![index])
+    .launch();
 }
