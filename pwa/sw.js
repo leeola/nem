10a2739b202488224731f23f673421b580c277cf
Lee Olayvar <leeola@fastmail.com>
2019-11-07 15:50:58 -0800

    feat: tweak log messages

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,26 +1,25 @@
-var cacheName = 'foo';
+var cacheName = 'nem-pwa';
 var appShellFiles = [
   '/assets/pwa/index.html',
 ];
 
 self.addEventListener('install', function(event) {
-  console.log('nem: installing pwa');
+  console.log('nem pwa: installing pwa');
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.log('nem: caching app shell and content');
+      console.log('nem pwa: caching app shell and content');
       return cache.addAll(appShellFiles);
     })
   );
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('nem: installing pwa');
   event.respondWith(
     caches.match(event.request).then(function(req) {
-      console.log('nem: fetching resource: ' + event.request.url);
+      console.log('nem pwa: fetching resource: ' + event.request.url);
       return req || fetch(event.request).then(function(response) {
         return caches.open(cacheName).then(function(cache) {
-          console.log('nem: caching new resource: ' + event.request.url);
+          console.log('nem pwa: caching new resource: ' + event.request.url);
           cache.put(event.request, response.clone());
           return response;
         });
@@ -30,12 +29,12 @@ self.addEventListener('fetch', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('nem: activating pwa');
+  console.log('nem pwa: activating pwa');
   event.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
         if (cacheName.indexOf(key) === -1) {
-          console.log('nem: deleting cache: ' + key);
+          console.log('nem pwa: deleting cache: ' + key);
           return caches.delete(key);
         }
       }));
