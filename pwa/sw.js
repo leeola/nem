580ff80546d1bc2953e0c56e1a41c0037c884753
Lee Olayvar <leeola@fastmail.com>
2019-11-07 18:11:52 -0800

    feat: improve caching in service worker and wasm build

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,13 +1,13 @@
-var cacheName = 'nem-pwa';
+var cacheName = 'nem-pwa-%%CACHE_IDENT%%';
 var appShellFiles = [
   '/assets/pwa/index.html',
 ];
 
 self.addEventListener('install', function(event) {
-  console.log('nem pwa: installing pwa');
+  console.log('nem pwa sw: installing pwa');
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.log('nem pwa: caching app shell and content');
+      console.log('nem pwa sw: caching app shell and content');
       return cache.addAll(appShellFiles);
     })
   );
@@ -16,10 +16,10 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(req) {
-      console.log('nem pwa: fetching resource: ' + event.request.url);
+      console.log('nem pwa sw: fetching resource: ' + event.request.url);
       return req || fetch(event.request).then(function(response) {
         return caches.open(cacheName).then(function(cache) {
-          console.log('nem pwa: caching new resource: ' + event.request.url);
+          console.log('nem pwa sw: caching new resource: ' + event.request.url);
           cache.put(event.request, response.clone());
           return response;
         });
@@ -29,12 +29,12 @@ self.addEventListener('fetch', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('nem pwa: activating pwa');
+  console.log('nem pwa sw: activating pwa');
   event.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
         if (cacheName.indexOf(key) === -1) {
-          console.log('nem pwa: deleting cache: ' + key);
+          console.log('nem pwa sw: deleting cache: ' + key);
           return caches.delete(key);
         }
       }));
--- server/build.rs
+++ server/build.rs
@@ -14,21 +14,46 @@ fn main() {
     fs::create_dir_all(&pwa_dst_path).unwrap();
 
     println!("cargo:rustc-env=PWA_DIR={}", pwa_dst_path.to_str().unwrap());
-    // rerun the build if any pwa files change.
-    ["index.html", "manifest.json", "sw.js", "src"]
-      .iter()
-      .for_each(|path| {
-        println!(
-          "cargo:rerun-if-changed={}",
-          pwa_crate_dir.join(path).to_str().unwrap()
-        );
-      });
 
-    ["index.html", "manifest.json", "sw.js"]
-      .iter()
-      .for_each(|file| {
-        fs::copy(pwa_crate_dir.join(file), pwa_dst_path.join(file)).unwrap();
-      });
+    // rerun the build if any pwa related files change.
+    [
+      "../pwa",
+      "../gui",
+      // TODO: the above dirs are not reloading properly. Fix this..
+      "../gui/src/lib.rs",
+      "../pwa/index.html",
+      "../pwa/manifest.json",
+      "../pwa/sw.js",
+    ]
+    .iter()
+    .for_each(|path| {
+      println!("cargo:rerun-if-changed={}", path);
+    });
+
+    ["index.html", "manifest.json"].iter().for_each(|file| {
+      fs::copy(pwa_crate_dir.join(file), pwa_dst_path.join(file)).unwrap();
+    });
+
+    // TODO: implement a better ident. Timestamp is just simple, but ideally it would be
+    // a content hash of some files. At this moment in time though i didn't want to hash
+    // all of the content in multiple crates :s. Perhaps i could base it off of cargo's
+    // identifier for the crates? Eg, the hash suffix on built packages.
+    {
+      use std::{
+        io::{Read, Write},
+        time::SystemTime,
+      };
+      let now = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+      let mut src = fs::File::open(pwa_crate_dir.join("sw.js")).unwrap();
+      let mut contents = String::new();
+      src.read_to_string(&mut contents).unwrap();
+      let contents = contents.replace("%%CACHE_IDENT%%", &format!("{}", now));
+      let mut dst = fs::File::create(pwa_dst_path.join("sw.js")).unwrap();
+      dst.write_all(contents.as_bytes()).unwrap();
+    }
 
     use wasm_pack::command::{
       build::{BuildMode, BuildOptions, Target},
