5d4c350f4a5ba8e7eb37d1db11bb6f6d9af801cb
Lee Olayvar <leeola@fastmail.com>
2019-11-08 19:44:52 -0800

    feat: testing recursion limit

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -1,6 +1,19 @@
 use {moxie, moxie_dom::*};
 
 #[topo::nested]
+pub fn base_layout() {
+  moxie::mox! {
+    <div>
+    <header role="banner">
+      <nav role="navigation">
+        <h1><a href="/">"Nem"</a></h1>
+      </nav>
+    </header>
+    </div>
+  }
+}
+
+#[topo::nested]
 pub fn mox_test() {
   let items = vec!["foo", "bar"];
   moxie::mox! {
--- pwa/src/lib.rs
+++ pwa/src/lib.rs
@@ -2,5 +2,5 @@ use {mnemosyne_gui::*, moxie_dom::*, wasm_bindgen::prelude::*};
 
 #[wasm_bindgen(start)]
 pub fn main() {
-  moxie_dom::boot(document().body().unwrap(), || mox_test!());
+  moxie_dom::boot(document().body().unwrap(), || base_layout!());
 }
