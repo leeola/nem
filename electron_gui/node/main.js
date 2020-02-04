bc97461ce2abb54543c0ada64d4086ca94facc8c
Lee Olayvar <leeola@fastmail.com>
2020-02-03 17:51:36 -0800

    feat: experimenting with tray

--- electron_gui/node/main.js
+++ electron_gui/node/main.js
@@ -4,10 +4,14 @@
 // With various tweaks to update to Electron 7.
 //
 
-let {app,  protocol,  BrowserWindow} = require("electron");
+let {app, protocol, BrowserWindow, Tray} = require("electron");
 let {readFile} = require("fs");
 let {extname} = require("path");
 let {URL} = require("url");
+let path = require("path");
+
+let tray = undefined
+let tratWindow = undefined
 
 let createProtocol = (scheme, normalize = true) => {
   protocol.registerBufferProtocol(scheme, (req, callback) => {
@@ -38,19 +42,90 @@ let createProtocol = (scheme, normalize = true) => {
 }
 
 protocol.registerSchemesAsPrivileged([{
-  scheme: 'app',
+  scheme: "app",
   privileges: { standard: true, secure: true, supportFetchAPI: true },
 }]);
 
 
 app.on("ready", () => {
   createProtocol("app");
-  let browserWindow = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true
-    }
-  });
-  // browserWindow.webContents.openDevTools();
-  browserWindow.loadFile("index.html");
+
+  // let browserWindow = new BrowserWindow({
+  //   webPreferences: {
+  //     nodeIntegration: false,
+  //     contextIsolation: true
+  //   }
+  // });
+  // // browserWindow.webContents.openDevTools();
+  // browserWindow.loadFile("index.html");
+
+  createTray();
+  createTrayWindow();
 });
+
+const createTray = () => {
+  tray = new Tray(`${__dirname}/../assets/tmp_icon.png`)
+  tray.on('right-click', toggleTrayWindow)
+  tray.on('double-click', toggleTrayWindow)
+  tray.on('click', function (event) {
+    toggleTrayWindow()
+
+    // Show devtools when command clicked
+    if (trayWindow.isVisible() && process.defaultApp && event.metaKey) {
+      //trayWindow.openDevTools({mode: 'detach'})
+    }
+  })
+}
+
+const createTrayWindow = () => {
+  trayWindow = new BrowserWindow({
+    width: 300,
+    height: 450,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false,
+    //transparent: true,
+    webPreferences: {
+      // Prevents renderer process code from not running when window is
+      // hidden
+      backgroundThrottling: false
+    }
+  })
+  let index_path = path.join(__dirname, "..", "browser", "tray_index.html");
+  trayWindow.loadURL(`file://${index_path}`)
+  // Hide the window when it loses focus
+  trayWindow.on("blur", () => {
+    if (!trayWindow.webContents.isDevToolsOpened()) {
+      trayWindow.hide()
+    }
+  })
+}
+
+const toggleTrayWindow = () => {
+  if (trayWindow.isVisible()) {
+    trayWindow.hide()
+  } else {
+    showWindow()
+  }
+}
+
+const showWindow = () => {
+  const position = getWindowPosition()
+  trayWindow.setPosition(position.x, position.y, false)
+  trayWindow.show()
+  trayWindow.focus()
+}
+
+const getWindowPosition = () => {
+  const windowBounds = trayWindow.getBounds()
+  const trayBounds = tray.getBounds()
+
+  // Center window horizontally below the tray icon
+  const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
+
+  // Position window 4 pixels vertically below the tray icon
+  const y = Math.round(trayBounds.y + trayBounds.height + 4)
+
+  return {x: x, y: y}
+}
